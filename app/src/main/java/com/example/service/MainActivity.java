6b4340c99fea3092faa55646421a91c96d0e9634
Ilya Desktop <ig@pss.one>
2020-02-17 11:30:04 +0300

    add service with live cycle log

--- app/src/main/java/com/example/service/MainActivity.java
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.service;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.main_runService)
+    Button runServicebtn;
+
+    @BindView(R.id.main_stopService)
+    Button stopServiceBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        runServicebtn.setOnClickListener(v -> {
+            startService();
+        });
+
+        stopServiceBtn.setOnClickListener(v -> {
+            stopService();
+        });
+    }
+
+    void startService(){
+        startService(new Intent(this,MyService.class));
+    }
+
+    void stopService(){
+        stopService(new Intent(this,MyService.class));
     }
 }
