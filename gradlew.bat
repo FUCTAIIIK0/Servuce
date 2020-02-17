8cdcea7367d95b72b73925fba6ce2e0e6d502e49
Ilya Desktop <ig@pss.one>
2020-02-17 10:45:18 +0300

    Start

---
+++ gradlew.bat
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="4">
+  <component name="AvailableLibraryUpdateStorage">
+    <library-updates>
+      <library-update>
+        <group-id>com.jakewharton</group-id>
+        <name>butterknife-compiler</name>
+        <stableOrPreviewVersion>10.2.1</stableOrPreviewVersion>
+        <stableVersion>10.2.1</stableVersion>
+        <repository>JCenter,Maven Central</repository>
+        <last-search-timestamp>1579622390789</last-search-timestamp>
+      </library-update>
+      <library-update>
+        <group-id />
+        <name>libuvccamera-release</name>
+        <repository />
+        <last-search-timestamp>1579622390789</last-search-timestamp>
+      </library-update>
+      <library-update>
+        <group-id />
+        <name>libsrtsender-release</name>
+        <repository />
+        <last-search-timestamp>1579622390789</last-search-timestamp>
+      </library-update>
+      <library-update>
+        <group-id>org.apache.commons</group-id>
+        <name>commons-lang3</name>
+        <stableOrPreviewVersion>3.9</stableOrPreviewVersion>
+        <stableVersion>3.9</stableVersion>
+        <repository>JCenter,Maven Central</repository>
+        <last-search-timestamp>1579622390789</last-search-timestamp>
+      </library-update>
+      <library-update>
+        <group-id />
+        <name>usbCameraCommon-release</name>
+        <repository />
+        <last-search-timestamp>1579622390789</last-search-timestamp>
+      </library-update>
+      <library-update>
+        <group-id>com.jakewharton</group-id>
+        <name>butterknife</name>
+        <stableOrPreviewVersion>10.2.1</stableOrPreviewVersion>
+        <stableVersion>10.2.1</stableVersion>
+        <repository>JCenter,Maven Central</repository>
+        <last-search-timestamp>1579622390789</last-search-timestamp>
+      </library-update>
+      <library-update>
+        <group-id>commons-net</group-id>
+        <name>commons-net</name>
+        <stableOrPreviewVersion>20030805.205232</stableOrPreviewVersion>
+        <stableVersion>20030805.205232</stableVersion>
+        <repository>JCenter,Maven Central</repository>
+        <last-search-timestamp>1579622390789</last-search-timestamp>
+      </library-update>
+     
